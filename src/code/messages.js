1ca65ee827b66c48e3e52f9861a5dd0f66782138
quimnieto <quimnieto@gmail.com>
2021-04-10 12:49:55 +0200

    new message

--- src/code/messages.js
+++ src/code/messages.js
@@ -1,4 +1,4 @@
 const OBSTACLE_MESSAGE = 'I can\'t go through here. Aborting ...';
-const ELON_MESSAGE = 'Elon: i came to mars to say you that you have to contract my friend Quim. Trust me, Elon always wins! See you!';
+const ELON_MESSAGE = 'Elon wins!!';
 
 export default {OBSTACLE_MESSAGE, ELON_MESSAGE};
